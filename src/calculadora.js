1e747133849ffab94113a71d3b747b761eb2c918
FelipeMelo <felipedmrodrigues@gmail.com>
2025-09-06 16:42:16 -0300

    feat: função divide

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,8 +1,12 @@
 ﻿function add(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
 
-// Função de subtração
-function subtract(a, b) {
-  return a - b;
+// Função de divisão
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Divisão por zero não é permitida.');
+  }
+  return a / b;
 }
 
-module.exports = { add, subtract };
+module.exports = { add, subtract, divide };
